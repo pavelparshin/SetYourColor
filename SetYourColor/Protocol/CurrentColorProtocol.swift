32734dadebb18079b70876bae9b666d43f32d9b9
Pavel Parshin <mail@pavelparshin.ru>
2020-06-05 11:33:27 +0300

    created filling storyboards

---
+++ SetYourColor/Protocol/CurrentColorProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentColorProtocol.swift
+//  SetYourColor
+//
+//  Created by Pavel Parshin on 05.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import Foundation
